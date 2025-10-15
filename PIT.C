2857b82911b6abd0c7565d452a637889d52fce49
zeopaclib0925-wq <zeopaclib0925@gmail.com>
2025-10-15 14:03:54 +0800

    Add files via upload
    Marktiane Paclib sir

---
+++ PIT.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    printf("Name: Marktiane Civsirk A. Paclib\n");
+    printf("ID Number: 2025304077\n");
+    printf("---------------------------\n");
+
+    
+    int num1 = 10, num2 = 20, sum;
+
+    sum = num1 + num2;
+
+    printf("Result: The sum of %d and %d is %d\n", num1, num2, sum);
+
+    return 0;
+}
